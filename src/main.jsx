d156d46402a6ef1b23c07e4bee341e272220bc8e
Rutwik Kalbandhe <83903631+Rutwik1@users.noreply.github.com>
2025-05-14 09:58:32 +0530

    Update React dependencies and fix imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import axios from 'axios'
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { Provider as ReduxProvider } from 'react-redux'
 import App from './App'
 import './index.css'
@@ -10,7 +10,9 @@ import { store } from './redux/store'
 // Update axios base URL to use HTTPS
 axios.defaults.baseURL = "https://api.openweathermap.org/data/2.5";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <ChakraProvider>
       <ReduxProvider store={store}>
